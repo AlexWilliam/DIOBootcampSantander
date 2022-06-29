caed1fd7bc8dcd149871b58bee7b41deb260de63
AlexWilliam <alexgoncalves.ti@gmail.com>
2022-06-29 20:02:23 -0300

    Sintaxe e operadores

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,19 +3,19 @@ function operacaoNumeros(num1, num2) {
 	let string = "";
 
 	if(num1 === num2){
-		string += "Os números "+num1+" e "+num2+" são iguais. ";
+		string += `Os números ${num1} e ${num2} são iguais. `;
 	}else{
-		string += "Os números "+num1+" e "+num2+" são diferentes. ";
+		string += `Os números ${num1} e ${num2} são diferentes. `;
 	}
 
 	let soma = num1 + num2;
 
 	if(soma > 10 && soma < 20){
-		string += "A soma dos números "+num1+" e "+num2+" é maior que 10 e menor que 20!";
+		string += `A soma dos números ${num1} e ${num2} é ${soma}, que é maior que 10 e menor que 20!`;
 	}else if(soma > 10 && soma > 20) {
-		string += "A soma dos números "+num1+" e "+num2+" é maior que 10 e maior que 20!"
+		string += `A soma dos números ${num1} e ${num2} é ${soma}, que é maior que 10 e maior que 20!`;
 	}else{
-		string += "A soma dos números "+num1+" e "+num2+" é menor que 10 e menor que 20!"
+		string += `A soma dos números ${num1} e ${num2} é ${soma}, que é menor que 10 e menor que 20!`;
 	}
 
 	return string+"\r\n";
